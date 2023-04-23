5ac2bae9787ef14afed83c37c3c2cb9db028ed3d
naomigonzalez <gon21043@byui.edu>
2023-04-22 23:53:55 -0600

    first draft

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+
+// document.querySelector("#lastUpdate").innerHTML=d;
+const lastModified = document.querySelector("#lastUpdate");
+// document.querySelector('lastUpdate').innerHTML = lastModified
+lastModified.innerHTML = document.lastModified;
+
+const currentTime = new Date();
+
+const year = currentTime.getFullYear();
+
+document.querySelector("#year").innerHTML = year;
+
